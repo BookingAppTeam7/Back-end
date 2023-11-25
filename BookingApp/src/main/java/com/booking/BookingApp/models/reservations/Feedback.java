aa6ea968e0437e28cbe60b9886eaa6c2e9239d9d
tamarazubac <105150962+tamarazubac@users.noreply.github.com>
2023-11-25 22:28:29 +0100

    Added constructors, getters and setters in model

--- BookingApp/src/main/java/com/booking/BookingApp/models/reservations/Feedback.java
+++ BookingApp/src/main/java/com/booking/BookingApp/models/reservations/Feedback.java
@@ -6,4 +6,34 @@ public class Feedback {
     public Long Id;
     public Long accommodationId;
     List<Review> reviews;
+
+    public Feedback(Long id, Long accommodationId, List<Review> reviews) {
+        Id = id;
+        this.accommodationId = accommodationId;
+        this.reviews = reviews;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Long getAccommodationId() {
+        return accommodationId;
+    }
+
+    public void setAccommodationId(Long accommodationId) {
+        this.accommodationId = accommodationId;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
--- BookingApp/src/main/java/com/booking/BookingApp/models/users/Notification.java
+++ BookingApp/src/main/java/com/booking/BookingApp/models/users/Notification.java
@@ -12,5 +12,51 @@ public class Notification {
     public String content;
     public LocalDateTime dateTime;
 
+    public Notification(Long id, Long userId, NotificationTypeEnum type, String content, LocalDateTime dateTime) {
+        this.id = id;
+        this.userId = userId;
+        this.type = type;
+        this.content = content;
+        this.dateTime = dateTime;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public NotificationTypeEnum getType() {
+        return type;
+    }
+
+    public void setType(NotificationTypeEnum type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 }
