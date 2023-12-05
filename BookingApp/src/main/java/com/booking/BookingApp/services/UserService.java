2d2efa65f6979749b45dfd054527a2bdf1a916e8
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2023-12-05 18:50:36 +0100

    Updated User Service
    Fixed bug with addresses in User constructor

--- BookingApp/src/main/java/com/booking/BookingApp/services/UserService.java
+++ BookingApp/src/main/java/com/booking/BookingApp/services/UserService.java
@@ -45,7 +45,7 @@ public class UserService implements IUserService{
         Long newId= (Long) counter.incrementAndGet();
         Map<NotificationTypeEnum,Boolean>notificationSettings=null;
 
-        User createdUser=new User(newUser.firstName, newUser.lastName,newUser.username, newUser.password, newUser.role,newUser.address,newUser.address, StatusEnum.DEACTIVE,newUser.reservationRequestNotification,
+        User createdUser=new User(newUser.firstName, newUser.lastName,newUser.username, newUser.password, newUser.role,newUser.address,newUser.phoneNumber, StatusEnum.DEACTIVE,newUser.reservationRequestNotification,
                 newUser.reservationCancellationNotification,newUser.ownerRatingNotification,newUser.accommodationRatingNotification,newUser.ownerRepliedToRequestNotification);
         return Optional.of(userRepository.save(createdUser));
     }
